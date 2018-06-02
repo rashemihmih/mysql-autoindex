1ee0e6bcd24e9953e4a9679fe8fa7e2831f2bf6a
rashemihmih <mihmih96@gmail.com>
2018-06-02 23:36:48 +0300

    Add actual algorithm

--- src/main/java/ru/bmstu/main/QueryAnalysis.java
+++ src/main/java/ru/bmstu/main/QueryAnalysis.java
@@ -12,6 +12,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class QueryAnalysis {
+    private static final String[] CONST_EXPRESSIONS = new String[] {"=", "<>", "!=", "is", "in"};
     private List<Table> tables;
     private Map<Table, List<Index>> queryIndexes = new HashMap<>();
 
@@ -26,6 +27,8 @@ public class QueryAnalysis {
                 .replaceAll("\\s*=\\s*", " = ")
                 .replaceAll("\\s*>=\\s*", " >= ")
                 .replaceAll("\\s*<=\\s*", " <= ")
+                .replaceAll("\\s*<>\\s*", " <> ")
+                .replaceAll("\\s*!=\\s*", " != ")
                 .replaceAll("\\s*,\\s*", ",");
         Map<Integer, List<Integer>> depthToSelectsStarts = depthToSelectStarts(query);
         Map<Integer, List<Pair<Integer, Integer>>> depthToBounds = depthToBounds(query, depthToSelectsStarts);
@@ -235,21 +238,108 @@ public class QueryAnalysis {
                 for (Table table : tables) {
                     indexes.put(table, new ArrayList<>());
                 }
-                Arrays.stream(where.split(" "))
-                        .map(s -> tableForColumn(s, tables))
-                        .filter(Objects::nonNull)
-                        .forEach(tableCol -> indexes.get(tableCol.getLeft()).add(tableCol.getRight()));
-                if (orderPos > 0) {
-                    String order = query.substring(orderPos + 9);
-                    Arrays.stream(order.split(" "))
-                            .map(s -> tableForColumn(s, tables))
-                            .filter(Objects::nonNull)
-                            .forEach(tableCol -> {
-                                List<String> indexCols = indexes.get(tableCol.getLeft());
-                                if (!indexCols.isEmpty() && !indexCols.contains(tableCol.getRight())) {
-                                    indexCols.add(tableCol.getRight());
+//                Arrays.stream(where.split(" "))
+//                        .map(s -> tableForColumn(s, tables))
+//                        .filter(Objects::nonNull)
+//                        .forEach(tableCol -> indexes.get(tableCol.getLeft()).add(tableCol.getRight()));
+//                if (orderPos > 0) {
+//                    String order = query.substring(orderPos + 9);
+//                    Arrays.stream(order.split(" "))
+//                            .map(s -> tableForColumn(s, tables))
+//                            .filter(Objects::nonNull)
+//                            .forEach(tableCol -> {
+//                                List<String> indexCols = indexes.get(tableCol.getLeft());
+//                                if (!indexCols.isEmpty() && !indexCols.contains(tableCol.getRight())) {
+//                                    indexCols.add(tableCol.getRight());
+//                                }
+//                            });
+//                }
+                String[] whereWords = where.split(" ");
+                for (int i = 0; i < whereWords.length - 1; i++) {
+                    String word = whereWords[i];
+                    Pair<Table, String> tableCol = tableForColumn(word, tables);
+                    if (tableCol != null) {
+                        String column = tableCol.getRight();
+                        if (isConstExpression(whereWords[i + 1])) {
+                            Table table = tableCol.getLeft();
+                            List<String> indexCols = indexes.get(table);
+                            if (!indexCols.contains(column)) {
+                                indexCols.add(column);
+                            }
+                        }
+                    }
+                }
+                boolean containsRange = false;
+                for (int i = 0; i < whereWords.length - 1 && !containsRange; i++) {
+                    String word = whereWords[i];
+                    Pair<Table, String> tableCol = tableForColumn(word, tables);
+                    if (tableCol != null) {
+                        String column = tableCol.getRight();
+                        if (!isConstExpression(whereWords[i + 1])) {
+                            Table table = tableCol.getLeft();
+                            List<String> indexCols = indexes.get(table);
+                            if (!indexCols.contains(column)) {
+                                indexCols.add(column);
+                                containsRange = true;
+                            }
+                        }
+                    }
+                }
+                if (!containsRange) {
+                    if (groupPos > 0) {
+                        String group;
+                        if (orderPos > 0) {
+                            group = query.substring(groupPos + 9, orderPos);
+                        } else {
+                            int limitPos = pos(query, "limit");
+                            if (limitPos > 0) {
+                                group = query.substring(groupPos + 9, limitPos);
+                            } else {
+                                group = query.substring(groupPos + 9);
+                            }
+                        }
+                        group = StringUtils.trim(group);
+                        for (String word : group.split(",")) {
+                            Pair<Table, String> tableCol = tableForColumn(word, tables);
+                            if (tableCol != null) {
+                                Table table = tableCol.getLeft();
+                                String column = tableCol.getRight();
+                                List<String> indexCols = indexes.get(table);
+                                if (!indexCols.contains(column)) {
+                                    indexCols.add(column);
                                 }
-                            });
+                            }
+                        }
+                    } else if (orderPos > 0) {
+                        List<Pair<Table, String>> orderTableCols = new ArrayList<>();
+                        String order = query.substring(orderPos + 9);
+                        boolean sortAsc = false;
+                        boolean sortDesc = false;
+                        for (String orderPart : order.split(",")) {
+                            String[] colOrder = orderPart.split(" ");
+                            Pair<Table, String> tableCol = tableForColumn(colOrder[0], tables);
+                            if (tableCol != null) {
+                                orderTableCols.add(tableCol);
+                                if (colOrder.length > 1 && "desc".equals(colOrder[1])) {
+                                    sortDesc = true;
+                                } else {
+                                    sortAsc = true;
+                                }
+                            }
+                            if (sortAsc && sortDesc) {
+                                break;
+                            }
+                        }
+                        if (!(sortAsc && sortDesc)) {
+                            for (Pair<Table, String> tableCol : orderTableCols) {
+                                String column = tableCol.getRight();
+                                List<String> indexCols = indexes.get(tableCol.getLeft());
+                                if (!indexCols.contains(column)) {
+                                    indexCols.add(column);
+                                }
+                            }
+                        }
+                    }
                 }
                 for (Map.Entry<Table, List<String>> entry : indexes.entrySet()) {
                     List<String> indexCols = entry.getValue();
@@ -262,28 +352,104 @@ public class QueryAnalysis {
         } else if (groupPos > 0) {
             String from = query.substring(fromPos + 5, groupPos);
             Map<Table, Set<String>> tableJoins = tableJoins(from);
-            if (tableJoins.size() > 1) {
-                addIndexesForJoins(indexes, tableJoins);
+//            if (tableJoins.size() > 1) {
+//                addIndexesForJoins(indexes, tableJoins);
+//            } else {
+//                Table table = tableJoins.keySet().stream().findAny().orElse(null);
+//                if (table == null) {
+//                    return indexes;
+//                }
+//                String group = query.substring(groupPos + 9);
+//                addIndexesForSubsequentColumns(indexes, group, table);
+//            }
+            List<Table> tables = new ArrayList<>(tableJoins.keySet());
+            for (Table table : tables) {
+                indexes.put(table, new ArrayList<>());
+            }
+            String group;
+            if (orderPos > 0) {
+                group = query.substring(groupPos + 9, orderPos);
             } else {
-                Table table = tableJoins.keySet().stream().findAny().orElse(null);
-                if (table == null) {
-                    return indexes;
+                int limitPos = pos(query, "limit");
+                if (limitPos > 0) {
+                    group = query.substring(groupPos + 9, limitPos);
+                } else {
+                    group = query.substring(groupPos + 9);
                 }
-                String group = query.substring(groupPos + 9);
-                addIndexesForSubsequentColumns(indexes, group, table);
+            }
+            group = StringUtils.trim(group);
+            Table firstTable = null;
+            for (String word : group.split(",")) {
+                Pair<Table, String> tableCol = tableForColumn(word, tables);
+                if (tableCol != null) {
+                    Table table = tableCol.getLeft();
+                    if (firstTable == null) {
+                        firstTable = table;
+                    } else if (!firstTable.equals(table)) {
+                        addIndexesForJoins(indexes, tableJoins);
+                        break;
+                    }
+                    String column = tableCol.getRight();
+                    List<String> indexCols = indexes.get(table);
+                    if (!indexCols.contains(column)) {
+                        indexCols.add(column);
+                    }
+                }
             }
         } else if (orderPos > 0) {
             String from = query.substring(fromPos + 4, orderPos);
             Map<Table, Set<String>> tableJoins = tableJoins(from);
-            if (tableJoins.size() > 1) {
-                addIndexesForJoins(indexes, tableJoins);
-            } else {
-                Table table = tableJoins.keySet().stream().findAny().orElse(null);
-                if (table == null) {
-                    return indexes;
+//            if (tableJoins.size() > 1) {
+//                addIndexesForJoins(indexes, tableJoins);
+//            } else {
+//                Table table = tableJoins.keySet().stream().findAny().orElse(null);
+//                if (table == null) {
+//                    return indexes;
+//                }
+//                String order = query.substring(orderPos + 9);
+//                addIndexesForSubsequentColumns(indexes, order, table);
+//            }
+            List<Table> tables = new ArrayList<>(tableJoins.keySet());
+            for (Table table : tables) {
+                indexes.put(table, new ArrayList<>());
+            }
+            List<Pair<Table, String>> orderTableCols = new ArrayList<>();
+            String order = query.substring(orderPos + 9);
+            boolean sortAsc = false;
+            boolean sortDesc = false;
+            Table firstTable = null;
+            boolean tablesMixed = false;
+            for (String orderPart : order.split(",")) {
+                String[] colOrder = orderPart.split(" ");
+                Pair<Table, String> tableCol = tableForColumn(colOrder[0], tables);
+                if (tableCol != null) {
+                    Table table = tableCol.getLeft();
+                    if (firstTable == null) {
+                        firstTable = table;
+                    } else if (!firstTable.equals(table)) {
+                        addIndexesForJoins(indexes, tableJoins);
+                        tablesMixed = true;
+                        break;
+                    }
+                    orderTableCols.add(tableCol);
+                    if (colOrder.length > 1 && "desc".equals(colOrder[1])) {
+                        sortDesc = true;
+                    } else {
+                        sortAsc = true;
+                    }
                 }
-                String order = query.substring(orderPos + 9);
-                addIndexesForSubsequentColumns(indexes, order, table);
+                if (sortAsc && sortDesc) {
+                    break;
+                }
+            }
+            if (!(sortAsc && sortDesc) && !tablesMixed) {
+                for (Pair<Table, String> tableCol : orderTableCols) {
+                    String column = tableCol.getRight();
+                    List<String> indexCols = indexes.get(tableCol.getLeft());
+                    if (!indexCols.contains(column)) {
+                        indexCols.add(column);
+                    }
+                }
             }
         } else {
             String from = query.substring(fromPos + 4);
@@ -293,19 +459,23 @@ public class QueryAnalysis {
         return indexes;
     }
 
-    private void addIndexesForSubsequentColumns(Map<Table, List<String>> indexes, String columns, Table table) {
-        List<String> indexCols = Arrays.stream(columns.split(","))
-                .map(col -> {
-                    for (String column : table.getColumns()) {
-                        if (column.equalsIgnoreCase(col)) {
-                            return column;
-                        }
-                    }
-                    return col;
-                })
-                .collect(Collectors.toList());
-        indexes.put(table, indexCols);
+    private boolean isConstExpression(String expression) {
+        return Arrays.stream(CONST_EXPRESSIONS).anyMatch(constExpression -> constExpression.equals(expression));
     }
+
+//    private void addIndexesForSubsequentColumns(Map<Table, List<String>> indexes, String columns, Table table) {
+//        List<String> indexCols = Arrays.stream(columns.split(","))
+//                .map(col -> {
+//                    for (String column : table.getColumns()) {
+//                        if (column.equalsIgnoreCase(col)) {
+//                            return column;
+//                        }
+//                    }
+//                    return col;
+//                })
+//                .collect(Collectors.toList());
+//        indexes.put(table, indexCols);
+//    }
 
     private void addIndexesForJoins(Map<Table, List<String>> indexes, Map<Table, Set<String>> tableJoins) {
         for (Map.Entry<Table, Set<String>> entry : tableJoins.entrySet()) {
